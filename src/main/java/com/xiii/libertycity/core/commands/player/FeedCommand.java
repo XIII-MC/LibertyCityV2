a5bac8dbf402201ab4da47aaa0912a5ca2dacc65
XIII___ <cyjallet@gmail.com>
2022-08-05 17:06:05 +0200

    (+) /feed

---
+++ src/main/java/com/xiii/libertycity/core/commands/player/FeedCommand.java
@@ -0,0 +1,31 @@
+package com.xiii.libertycity.core.commands.player;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FeedCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(command.getName().equalsIgnoreCase("feed")) {
+            if(args.length == 0) {
+                Player p = (Player) sender;
+                p.setFoodLevel(20);
+                p.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été rassasié");
+            } else {
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+                if(target.isOnline()) {
+                    target.setFoodLevel(20);
+                    target.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été rassasié");
+                    sender.sendMessage("§2§lLiberty§a§lCity §7» §e" + target.getName() + " §fa été rassasié");
+                } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! " + target.getName() + " n'est pas en ligne!");
+            }
+        }
+
+        return true;
+    }
+}
