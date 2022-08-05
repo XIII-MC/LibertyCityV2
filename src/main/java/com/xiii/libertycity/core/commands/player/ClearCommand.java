f257ace0bc874791d4bf27d805c9f0640e184e3a
XIII___ <cyjallet@gmail.com>
2022-08-05 17:03:51 +0200

    (+) /clear

---
+++ src/main/java/com/xiii/libertycity/core/commands/player/ClearCommand.java
@@ -0,0 +1,31 @@
+package com.xiii.libertycity.core.commands.player;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ClearCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
+
+        if(command.getName().equalsIgnoreCase("clear")) {
+            if(args.length == 0) {
+                p.getInventory().clear();
+                p.sendMessage("§2§lLiberty§a§lCity §7» §fVotre inventaire a été vidé");
+            } else {
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+                if(target.isOnline()) {
+                    target.getInventory().clear();
+                    target.sendMessage("§2§lLiberty§a§lCity §7» §fVotre inventaire a été vidé");
+                    p.sendMessage("§2§lLiberty§a§lCity §7» Inventaire de §e" + target.getName() + " §fa été vidé");
+                } else p.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! " + target.getName() + " n'est pas en ligne!");
+            }
+        }
+
+        return true;
+    }
+}
