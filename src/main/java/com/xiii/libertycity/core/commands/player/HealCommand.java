67c9298bc9d78c206d2369233e65f172082b9125
XIII___ <cyjallet@gmail.com>
2022-08-05 17:08:12 +0200

    (+) /heal

---
+++ src/main/java/com/xiii/libertycity/core/commands/player/HealCommand.java
@@ -0,0 +1,31 @@
+package com.xiii.libertycity.core.commands.player;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HealCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(command.getName().equalsIgnoreCase("heal")) {
+            if(args.length == 0) {
+                Player p = (Player) sender;
+                p.setHealth(20);
+                p.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été soigné");
+            } else {
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+                if(target.isOnline()) {
+                    target.setHealth(20);
+                    target.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été soigné");
+                    sender.sendMessage("§2§lLiberty§a§lCity §7» §e" + target.getName() + " §fa été soigné");
+                } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! " + target.getName() + " n'est pas en ligne!");
+            }
+        }
+
+        return true;
+    }
+}
