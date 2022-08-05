3470b9fdbf77310ae791a394035855a519f5fa89
XIII___ <cyjallet@gmail.com>
2022-08-05 17:15:40 +0200

    (+) TimeUtil

---
+++ src/main/java/com/xiii/libertycity/core/utils/TimeUtil.java
@@ -0,0 +1,15 @@
+package com.xiii.libertycity.core.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeUtil {
+
+    public static String getDate() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return (dtf.format(now));
+    }
+
+}
+
