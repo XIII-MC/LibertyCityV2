affffbbe4e8b938f8a76d24fcbf2547c81b1c3e8
XIII___ <cyjallet@gmail.com>
2022-08-05 17:06:47 +0200

    (+) /fly

---
+++ src/main/java/com/xiii/libertycity/core/commands/player/FlyCommand.java
@@ -0,0 +1,42 @@
+package com.xiii.libertycity.core.commands.player;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FlyCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(command.getName().equalsIgnoreCase("fly")) {
+            if(args.length == 0) {
+                Player p = (Player) sender;
+                if(p.getAllowFlight()) {
+                    p.setAllowFlight(true);
+                    p.sendMessage("§2§lLiberty§a§lCity §7» §fVous pouvez désormais volé");
+                } else {
+                    p.setAllowFlight(false);
+                    p.sendMessage("§2§lLiberty§a§lCity §7» §fVous ne pouvez plus volé");
+                }
+            } else {
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+                if(target.isOnline()) {
+                    if(target.getAllowFlight()) {
+                        target.setAllowFlight(true);
+                        target.sendMessage("§2§lLiberty§a§lCity §7» §fVous pouvez désormais volé");
+                        sender.sendMessage("§2§lLiberty§a§lCity §7» §e" + target.getName() + " §fpeut désormais volé");
+                    } else {
+                        target.setAllowFlight(false);
+                        target.sendMessage("§2§lLiberty§a§lCity §7» §fVous ne pouvez plus volé");
+                        sender.sendMessage("§2§lLiberty§a§lCity §7» §e" + target.getName() + " §fne peut plus volé");
+                    }
+                } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! " + target.getName() + " n'est pas en ligne!");
+            }
+        }
+
+        return true;
+    }
+}
