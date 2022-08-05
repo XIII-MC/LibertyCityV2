ddb4e577072f19f04d741a05d177c129fd2e42f3
XIII___ <cyjallet@gmail.com>
2022-08-05 17:15:10 +0200

    (+) /tp

---
+++ src/main/java/com/xiii/libertycity/core/commands/player/TeleportCommand.java
@@ -0,0 +1,48 @@
+package com.xiii.libertycity.core.commands.player;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TeleportCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(command.getName().equalsIgnoreCase("teleport")) {
+            if (args.length == 0) sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! Usage: /teleport <Joueur> (Joueur)");
+            else {
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+                Player p = (Player) sender;
+                if(target.isOnline()) {
+                    if(args.length == 1) {
+                        p.teleport(target);
+                        p.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été téléporter à §e" + target.getName());
+                    } else if(args.length == 2) {
+                        Player target2 = Bukkit.getServer().getPlayer(args[1]);
+                        if(target2.isOnline()) {
+                            target.teleport(target2);
+                            target.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été téléporter à §e" + target2.getName());
+                            p.sendMessage("§2§lLiberty§a§lCity §7» §e" + target.getName() + " §fa été téléporter à §e" + target2.getName());
+                        } else p.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! " + target.getName() + " n'est pas en ligne!");
+                    }
+                } else p.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! " + target.getName() + " n'est pas en ligne!");
+            }
+        }
+
+        if(command.getName().equalsIgnoreCase("tpall")) {
+            for(Player p : Bukkit.getOnlinePlayers()) {
+                p.teleport((Player) sender);
+                p.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été téléporter vers §e" + sender.getName());
+            }
+        }
+
+        if(command.getName().equalsIgnoreCase("tphere")) {
+
+        }
+
+        return true;
+    }
+}
