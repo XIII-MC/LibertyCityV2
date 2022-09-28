95801a81e5f39cbaad9fc731ae5d4174d2591a46
XIII___ <cyjallet@gmail.com>
2022-09-28 08:11:35 +0200

    (+) /repair

--- src/main/java/com/xiii/libertycity/core/commands/player/RepairCommand.java
+++ src/main/java/com/xiii/libertycity/core/commands/player/RepairCommand.java
@@ -1,6 +1,27 @@
 package com.xiii.libertycity.core.commands.player;
 
+import org.bukkit.Material;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class RepairCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(command.getName().equalsIgnoreCase("repair")) {
+            Player snder = (Player) sender;
+            if(snder.getItemInHand() != null && snder.getItemInHand() != new ItemStack(Material.AIR)) {
+                snder.getItemInHand().setDurability((short)0);
+                sender.sendMessage("§2§lLiberty§a§lCity §7» §fVotre item a été réparé");
+            } else {
+                sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Item invalide");
+            }
+        }
+
+        return true;
+    }
 }
