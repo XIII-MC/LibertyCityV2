d6fae6090f651f09ec56147b101be60e58ba8415
XIII___ <cyjallet@gmail.com>
2022-09-28 08:10:41 +0200

    (+) /near

--- src/main/java/com/xiii/libertycity/core/commands/player/NearCommand.java
+++ src/main/java/com/xiii/libertycity/core/commands/player/NearCommand.java
@@ -6,5 +6,32 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NearCommand implements CommandExecutor {
+
+    List<String> playersWithinRange = new ArrayList<>();
+    Integer range;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(command.getName().equalsIgnoreCase("near")) {
+            if(args.length >= 1) {
+                range = Integer.valueOf(args[0]);
+            } else range = 20;
+            Player snder = (Player) sender;
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                if (p.getLocation().distance(snder.getLocation()) <= range) {
+                    playersWithinRange.add(p.getName());
+                }
+            }
+            if (playersWithinRange.size() > 0) {
+                sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + playersWithinRange.size() + " §fjoueurs trouvés dans un rayon de §6" + range + " §f: " + playersWithinRange);
+            } else sender.sendMessage("§2§lLiberty§a§lCity §7» §fAucun joueur trouvé dans un rayon de §6" + range);
+        }
+
+        return true;
+    }
 }
