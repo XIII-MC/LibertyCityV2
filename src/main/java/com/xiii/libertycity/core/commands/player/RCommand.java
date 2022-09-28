b57c12508bbd170459777dcd63b54ef07903ce39
XIII___ <cyjallet@gmail.com>
2022-09-28 08:11:21 +0200

    (~) /r

--- src/main/java/com/xiii/libertycity/core/commands/player/RCommand.java
+++ src/main/java/com/xiii/libertycity/core/commands/player/RCommand.java
@@ -25,7 +25,10 @@ public class RCommand implements CommandExecutor {
                             for (int i = 0; i < args.length; i++) {
                                 message += args[i] + " ";
                             }
-                            data.lastDm.sendMessage("§7[§a§l" + send.rpPrenom + " §2§l" + send.rpNom + " §7(" + sender.getName() + "§7)" + "§r §6--> §a§l" + tar.rpPrenom + " §2§l" + tar.rpNom + " §7(" + data.lastDm.getName() + ")] §f" + message);
+                            sender.sendMessage("§7[§a§lVous§7" + "§r §6--> §a§l" + tar.rpPrenom + " §2§l" + tar.rpNom + " §7(" + data.lastDm.getName() + ")] §f" + message);
+                            if(!tar.ignoredPlayers.contains(sender.getName())) {
+                                data.lastDm.sendMessage("§7[§a§l" + send.rpPrenom + " §2§l" + send.rpNom + " §7(" + sender.getName() + "§7)" + "§r §6--> §a§lMoi§7] §f" + message);
+                            }
                         } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cCette personne n'autorise pas les messages privé");
                     } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! " + data.lastDm.getName() + " n'est pas en ligne!");
                 }
