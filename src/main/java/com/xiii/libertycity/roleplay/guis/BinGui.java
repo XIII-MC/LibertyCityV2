e4fe968668aec53e32c5253f020cb2afe7a067f2
XIII___ <cyjallet@gmail.com>
2022-10-09 16:09:44 +0200

    Ajout de la poubelle

---
+++ src/main/java/com/xiii/libertycity/roleplay/guis/BinGui.java
@@ -0,0 +1,24 @@
+package com.xiii.libertycity.roleplay.guis;
+
+import com.xiii.libertycity.LibertyCity;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+
+public class BinGui implements Listener {
+
+    @EventHandler
+    public void onBinInteract(PlayerInteractEvent e) {
+        Bukkit.getScheduler().scheduleSyncDelayedTask(LibertyCity.INSTANCE, () -> {
+
+            if(e.getClickedBlock().getTypeId() == 4665) {
+                Inventory bin = Bukkit.createInventory(e.getPlayer(), 54, "§8Poubelle");
+                e.getPlayer().openInventory(bin);
+            }
+
+        });
+    }
+
+}
