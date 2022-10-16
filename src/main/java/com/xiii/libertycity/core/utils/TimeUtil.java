37d12f62772703baf9b0483700d9944ce57bf337
XIII___ <cyjallet@gmail.com>
2022-10-16 21:45:32 +0200

    Ajout d'une commande speciale pour le test

--- src/main/java/com/xiii/libertycity/core/utils/TimeUtil.java
+++ src/main/java/com/xiii/libertycity/core/utils/TimeUtil.java
@@ -1,13 +1,29 @@
 package com.xiii.libertycity.core.utils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
 
-    public static String getDate() {
+    public static String getFullDate() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
+        return (dtf.format(now));
+    }
+
+    public static String getDate() {
+        DateFormat dtf = new SimpleDateFormat("dd/MM/yyyy");
+        LocalDate now = LocalDate.now();
+        return (dtf.format(now));
+    }
+
+    public static String getTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime now = LocalTime.now();
         return (dtf.format(now));
     }
 
