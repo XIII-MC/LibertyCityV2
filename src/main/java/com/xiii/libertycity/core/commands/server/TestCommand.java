c650ee3fd794916c9f8cb0bc64e0cf79ee6bdbb8
XIII___ <cyjallet@gmail.com>
2022-10-16 21:45:44 +0200

    Ajout d'une commande speciale pour le test

---
+++ src/main/java/com/xiii/libertycity/core/commands/server/TestCommand.java
@@ -0,0 +1,33 @@
+package com.xiii.libertycity.core.commands.server;
+
+import com.xiii.libertycity.core.data.Data;
+import com.xiii.libertycity.core.data.PlayerData;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TestCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(command.getName().equalsIgnoreCase("LCDBB1BETA")) {
+
+            // Initialize player and data.
+            Player player = (Player) sender;
+            PlayerData playerData = Data.data.getUserData(player);
+
+            // Previous tests list :
+
+                // - TEST_1 : testing PlayerData flexibility to new variables | SUCCESS.
+
+            // Test section. | CASE: ???? : ????
+
+
+
+        }
+
+        return true;
+    }
+}
