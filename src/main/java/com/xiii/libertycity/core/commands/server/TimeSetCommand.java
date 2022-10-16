2eb24f0b37c533bfaed7e2601767b2a186d530c9
XIII___ <cyjallet@gmail.com>
2022-10-16 21:45:57 +0200

    Ajout du /time

--- src/main/java/com/xiii/libertycity/core/commands/server/TimeSetCommand.java
+++ src/main/java/com/xiii/libertycity/core/commands/server/TimeSetCommand.java
@@ -1,6 +1,7 @@
 package com.xiii.libertycity.core.commands.server;
 
 import com.xiii.libertycity.core.utils.AlertUtil;
+import com.xiii.libertycity.core.utils.TimeUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,6 +21,10 @@ public class TimeSetCommand implements CommandExecutor {
         if(command.getName().equalsIgnoreCase("night")) {
             p.getWorld().setTime(13000);
             AlertUtil.staffAlert("§8" + p.getName() + " §7a mis l'heure a §8Nuit", "LibertyCity.staff.alert", 0);
+        }
+
+        if(command.getName().equalsIgnoreCase("time")) {
+            p.sendMessage("§2§lLiberty§a§lCity §7» §fIl est actuellement §6" + p.getWorld().getTime() + " §7(" + TimeUtil.getTime() + ")");
         }
 
         return true;
