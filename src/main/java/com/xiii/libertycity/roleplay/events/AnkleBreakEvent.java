581cc528676fbfb12c6aa0a0ab18a48aac381df1
XIII___ <cyjallet@gmail.com>
2022-10-19 16:50:50 +0200

    Refonte TOTAL du systeme de cheville foulée

--- src/main/java/com/xiii/libertycity/roleplay/events/AnkleBreakEvent.java
+++ src/main/java/com/xiii/libertycity/roleplay/events/AnkleBreakEvent.java
@@ -2,42 +2,42 @@ package com.xiii.libertycity.roleplay.events;
 
 import com.xiii.libertycity.LibertyCity;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.Random;
-
 public class AnkleBreakEvent implements Listener {
 
-    private int chancePercentage = 0;
+    private double fallDistance;
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void forFallDistanceCalculation(PlayerMoveEvent e) {
+        Bukkit.getScheduler().runTaskAsynchronously(LibertyCity.INSTANCE, () -> {
+            if(e.getPlayer().getFallDistance() > 0) fallDistance = e.getPlayer().getFallDistance();
+        });
+    }
 
     @EventHandler
     public void forAnkleBreak(EntityDamageEvent e) {
-        Bukkit.getScheduler().scheduleSyncDelayedTask(LibertyCity.INSTANCE, () -> {
+        Bukkit.getScheduler().runTaskAsynchronously(LibertyCity.INSTANCE, () -> {
 
             if(e.getEntity() instanceof Player) {
                 if(e.getCause() == EntityDamageEvent.DamageCause.FALL) {
 
-                    int a = new Random().nextInt(10);
-                    if(a < 60) {
-                        chancePercentage = 60;
-                    } else if (a < 9) {
-                        chancePercentage = 30;
-                    } else {
-                        chancePercentage = 10;
-                    }
+                    double a = Math.random() * 100;
 
-                    if(chancePercentage <= 100) {
-                        PotionEffect potionEffect = new PotionEffect(PotionEffectType.SLOW, 30, 2);
-                        potionEffect.apply((LivingEntity) e.getEntity());
-                        PotionEffect potEffect = new PotionEffect(PotionEffectType.BLINDNESS, 3, 2);
-                        potEffect.apply((LivingEntity) e.getEntity());
-                        e.getEntity().sendMessage("§4(⚠) §cVous vous êtes foulé une cheville !");
+                    if(a <= 30 || fallDistance >= 4.1f) {
+                        Bukkit.getScheduler().runTask(LibertyCity.INSTANCE, () -> {
+                            ((Player) e.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, (int) (100 * fallDistance), (int) fallDistance / 2));
+                            ((Player) e.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 50, 2));
+                            ((Player) e.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.JUMP, (int) (100 * fallDistance), 250));
+                        });
+                        e.getEntity().sendMessage("§4§l⚠ §cVous vous êtes foulé une cheville !");
                     }
 
                 }
